2d76d8f2361a4f034c4f86f1b8652954bd9ed727
Bolo BASSOUM <bassoumbolo078@gmail.com>
2026-09-10 13:50:00 +0000

    chore: initialise le projet Maven (quickstart)

---
+++ src/test/java/fr/formation/reservation/AppTest.java
@@ -0,0 +1,20 @@
+package fr.formation.reservation;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
